21cfe9e67804cc8860fa7a3cbf1211293be96703
hotolla <86011142+hotolla@users.noreply.github.com>
2022-06-21 21:13:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux/es/exports";
+import store from "./redux/store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const store = useSelector(store => store);
+  console.log(store);
+  
+  return (<h1>Hello word!!!</h1>)
 }
 
 export default App;
